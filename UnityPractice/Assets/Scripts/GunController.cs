b432f0c13969e4c9352226a1bf00e3c022544672
konglobemeralt <jesper.blidkvist@live.se>
2017-06-24 15:37:58 +0200

    Added rotation ablities and a gun

---
+++ UnityPractice/Assets/Scripts/GunController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public Gun startingGun;
+    public Transform weaponHold;
+    private Gun equippedGun;
+
+    void Start() {
+        if (startingGun != null) {
+            EquipGun(startingGun);
+                }
+    }
+
+    public void EquipGun(Gun gunToEquip) {
+        if (equippedGun != null) {
+            Destroy(equippedGun.gameObject);
+        }
+        equippedGun = Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation) as Gun;
+        equippedGun.transform.parent = weaponHold;
+
+    }
+} 
