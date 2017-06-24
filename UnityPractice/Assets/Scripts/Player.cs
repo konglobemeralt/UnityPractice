ab78d47cf8b738a4050cc05ecaa1db4ce9d1158e
konglobemeralt <jesper.blidkvist@live.se>
2017-06-24 15:00:46 +0200

    Added basic scene and script for basic movement

---
+++ UnityPractice/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(PlayerControler))]
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5;
+    PlayerControler controller;
+
+	void Start () {
+        controller = GetComponent<PlayerControler>();	
+	}
+	
+	void Update () {
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+	}
+}
