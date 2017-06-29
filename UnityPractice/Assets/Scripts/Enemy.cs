48f2e20179f04d507d2cc71add8a6b71c8768740
konglobemeralt <jesper.blidkvist@live.se>
2017-06-29 11:31:02 +0200

    Finished ep 3 collision layers osv

--- UnityPractice/Assets/Scripts/Enemy.cs
+++ UnityPractice/Assets/Scripts/Enemy.cs
@@ -12,10 +12,23 @@ public class Enemy : MonoBehaviour {
 	void Start () {
         pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
 	}
 	
 	// Update is called once per frame
 	void Update () {
         pathfinder.SetDestination(target.position);
 	}
+
+    IEnumerator UpdatePath() {
+        float refreshRate = 0.25f;
+
+        while (target != null) {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+
+        }
+    }
 }
--- UnityPractice/Assets/Scripts/Projectile.cs
+++ UnityPractice/Assets/Scripts/Projectile.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
-    float speed = 10; 
-
+    float speed = 10;
+    public LayerMask collisionMask;
 
     public void SetSpeed(float newSpeed) {
         speed = newSpeed;
     }
 
 	void Update () {
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 	}
+
+    void CheckCollisions(float moveDistance) {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide)) {
+            OnHitObject(hit);
+        }
+    }
+
+
+    void OnHitObject(RaycastHit hit) {
+        print(hit.collider.gameObject.name);
+        GameObject.Destroy(gameObject);
+    }
 }
